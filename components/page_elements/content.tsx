495664873fc99e1a0dfe8cac9a1efb5404805f24
duechayapolgmit <duechayapolsbs@gmail.com>
2026-07-09 22:08:48 +0100

    feat: ability to add images to content

--- components/page_elements/content.tsx
+++ components/page_elements/content.tsx
@@ -1,9 +1,19 @@
 export default function Content(
-    {header = "", content, background = "white"}: {header?: string, content: any, background?: string}) 
+    {header = "", content, background = "white", backgroundImage = ""}: 
+    {header?: string, content: any, background?: string, backgroundImage?: string}) 
 {
+    const getClassName = () => {
+        if (backgroundImage) return "bg-black/75 bg-blend-overlay text-white p-6"
+        if (background) return "bg-white text-black p-6";
+    }
+    const getStyle = () => {
+        if (backgroundImage) return {backgroundImage: `url('${backgroundImage}'`};
+        if (background) return {backgroundColor: background};
+    }
+
     return (
-        <div className="bg-white text-black p-6"
-            style={{backgroundColor: background}}>
+        <div className={getClassName()}
+            style={getStyle()}>
             {header == "" ? "" : 
                 <h1 className="font-metropolis-black text-5xl uppercase pb-6">
                     {header}
